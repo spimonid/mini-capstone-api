645ce8455cdb6bb14cf3b3dc7f37ad079f42e8a8
Dan Tuls <spimonid@gmail.com>
2021-05-04 19:47:49 -0500

    created all_products route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/products", controller: products, action: "all_products"
+  get "/all_products", controller: "products", action: "all_products"
 end
