0097839dee270ba38f8684b53dcef1cb26a947f7
Dan Tuls <spimonid@gmail.com>
2021-05-10 20:26:24 -0500

    add migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_235527) do
+ActiveRecord::Schema.define(version: 2021_05_11_005139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "title"
-    t.integer "price"
+    t.decimal "price", precision: 5, scale: 2
     t.string "image_url"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "inventory"
   end
-
 end
